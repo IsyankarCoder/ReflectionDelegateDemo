5c81d88fb6af5ca5cc69896fd2fe00969a04ae68
isyankarcoder <gencvolkan@gmail.com>
2020-12-26 17:02:59 +0300

    ..

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReflectionDelegateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
